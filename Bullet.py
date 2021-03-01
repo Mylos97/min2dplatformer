64bfcb4a7cbd86e1df8ef514da23c005ddd64a29
Mylos97 <57527830+Mylos97@users.noreply.github.com>
2021-03-01 15:15:28 +0100

    bullet

---
+++ Bullet.py
@@ -0,0 +1,18 @@
+
+
+class Bullet(GameObject):
+
+    def __init__(self, screen, xInput, yInput, objectID, mediator):
+        self.screen = screen
+        self.xInput = xInput
+        self.yInput = yInput
+        self.objectID = objectID
+        self.mediator = mediator
+        self.img = 
+
+
+    def loop():
+        pass
+
+    def draw():
+        pass
